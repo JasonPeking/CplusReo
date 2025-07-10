e148cdf9461fc7f0e7c31b5960800371aac3cd05
Silve <davidcoryytw0vt@gmail.com>
2025-07-11 00:03:49 +0800

    plan

--- Qt/Ex9/main.cpp
+++ Qt/Ex9/main.cpp
@@ -1,11 +1,8 @@
-#include "mainwindow.h"
-
 #include <QApplication>
-
+#include<iostream>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    //test
     return a.exec();
 }
